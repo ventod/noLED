c67e4f8a56a1800498f35d0c3226d19487806fc3
Vento <>
2021-02-02 20:19:08 +0100

    Autoformat + Clear cache

--- main.c
+++ main.c
@@ -8,24 +8,27 @@
 
 #define NO_ERROR 0
 
-#define MAKE_DUMMY_FUNCTION_RETURN_0(a) do {\
-	_sw(0x03E00008, a);\
-	_sw(0x00001021, a + 4);\
-} while (0) // from utils.h procfw 
+#define MAKE_DUMMY_FUNCTION_RETURN_0(a) \
+	do                                  \
+	{                                   \
+		_sw(0x03E00008, a);             \
+		_sw(0x00001021, a + 4);         \
+	} while (0) // from utils.h procfw
 
-#define SCE_ERROR_OK 0 // from errors.h uOFW
+#define SCE_ERROR_OK 0					   // from errors.h uOFW
 #define POWER_CALLBACK_MAX_SLOTS_KERNEL 31 // from power.c uOFW
-#define SCE_LED_MODE_OFF 0 // from led.h uOFW
+#define SCE_LED_MODE_OFF 0				   // from led.h uOFW
 
-typedef struct {
-    u8 selectiveCmd;
-    u8 numExecs;
-    u8 customCmd;
-    u8 unk;
-    s32 onTime; 
-    s32 offTime; 
-    s32 blinkTime; 
-    s32 endBlinkState; 
+typedef struct
+{
+	u8 selectiveCmd;
+	u8 numExecs;
+	u8 customCmd;
+	u8 unk;
+	s32 onTime;
+	s32 offTime;
+	s32 blinkTime;
+	s32 endBlinkState;
 } SceLedConfiguration; // from led.h uOFW
 
 PSP_MODULE_INFO("noLED", 0x1000, 1, 0);
@@ -51,22 +54,26 @@ int module_start(SceSize args UNUSED, void *argp UNUSED)
 	if (address)
 	{
 		// as function
-		s32 (*sceLedSetMode)(s32, s32, SceLedConfiguration *) = (void *) address;
+		s32 (*sceLedSetMode)(s32, s32, SceLedConfiguration *) = (void *)address;
 		// turn off leds
 		sceLedSetMode(0, SCE_LED_MODE_OFF, NULL); // MS
 		sceLedSetMode(1, SCE_LED_MODE_OFF, NULL); // WLAN
 		sceLedSetMode(3, SCE_LED_MODE_OFF, NULL); // BT
 		// patch sceLedSetMode
 		MAKE_DUMMY_FUNCTION_RETURN_0(address);
+		// clear caches
+		sceKernelDcacheWritebackAll();
+		sceKernelIcacheClearAll();
 	}
 	// also remove power from leds
 	noLedPower(0, PSP_POWER_CB_RESUME_COMPLETE, NULL);
-	// create and register callback to 
+	// create and register callback to
 	SceUID cbid = sceKernelCreateCallback("noLedPower", noLedPower, NULL);
 	if (cbid)
 	{
 		s32 status = !SCE_ERROR_OK;
-		for (s32 slot = POWER_CALLBACK_MAX_SLOTS_KERNEL; slot >= 0 && status != SCE_ERROR_OK; --slot) {
+		for (s32 slot = POWER_CALLBACK_MAX_SLOTS_KERNEL; slot >= 0 && status != SCE_ERROR_OK; --slot)
+		{
 			status = scePowerRegisterCallback(slot, cbid);
 		}
 	}
